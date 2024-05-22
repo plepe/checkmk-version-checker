e978778dca15f207f129d3b706cce7bfe3b00b69
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2024-05-22 11:25:31 +0200

    functions.sh: evaluate versions in central function

---
+++ functions.sh
@@ -0,0 +1,27 @@
+# Parameters:
+# - Name of service (e.g. My Homepage)
+# - Name of application (e.g. Wordpress)
+# - List of supported versions, most current version first (CR separated) (e.g. "1.40.0\n1.39.5\n1.38.9")
+# - Currently installed version (e.g. "1.39.4")
+evaluate () {
+  local SERVICE=${1}
+  local APP=${2}
+  local SUPPORTED_VERSIONS=${3}
+  local CURRENT_VERSION=${4}
+
+  local LATEST_VERSION=$(echo "${SUPPORTED_VERSIONS}" | head -n1 | xargs)
+  local CURRENT_MINOR=$(echo ${CURRENT_VERSION} | cut -d\. -f1-2 | xargs)
+  local LATEST_MINOR=$(echo "${SUPPORTED_VERSIONS}" | grep "^${CURRENT_MINOR}\." | head -n1 | xargs)
+
+  if [ "${CURRENT_VERSION}" = "${LATEST_VERSION}" ] ; then
+    echo "0 \"${APP} version ${SERVICE}\" - Current version: ${CURRENT_VERSION}"
+  elif [ "${CURRENT_VERSION}" = "${LATEST_MINOR}" ] ; then
+    echo "0 \"${APP} version ${SERVICE}\" - Current version: ${CURRENT_VERSION} (still supported), available: ${LATEST_VERSION}"
+  elif [ "${LATEST_MINOR}" = "" ] || [ "${LATEST_MINOR}" = "${LATEST_VERSION}" ] ; then
+    echo "1 \"${APP} version ${SERVICE}\" - Current version: ${CURRENT_VERSION}, available: ${LATEST_VERSION}"
+  else
+    echo "1 \"${APP} version ${SERVICE}\" - Current version: ${CURRENT_VERSION}, available: ${LATEST_VERSION} or ${LATEST_MINOR}"
+  fi
+}
+
+#evaluate "My Homepage" "Wordpress" "1.40.0\n1.39.5\n1.38.9" "1.38.9"
